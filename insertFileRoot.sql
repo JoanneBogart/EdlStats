b44a644f3957812a726eb87afb1b273c0bbbab81
Joanne Bogart <jrb@slac.stanford.edu>
2018-06-01 12:51:51 -0700

    Small db schema changes. Python code can now do initial file scan and store info in db

--- insertFileRoot.sql
+++ insertFileRoot.sql
@@ -1,2 +1,7 @@
-insert into FileRoot (dir, nickname, version) values ('/reg/g/pcds/package/epics/3.14-dev/screeens/edm/xpp/current', 'xpp','3.14-dev');
-insert into FileRoot (dir, nickname, version) values ('/reg/g/pcds/package/epics/3.14-dev/screeens/edm/mec/current', 'mec','3.14-dev')
+insert into FileRoot (dir, nickname, version) values ('/reg/g/pcds/package/epics/3.14-dev/screens/edm/xpp/current', 'xpp','3.14-dev');
+insert into FileRoot (dir, nickname, version) values ('/reg/g/pcds/package/epics/3.14-dev/screens/edm/mec/current', 'mec','3.14-dev')
+
+insert into FileRoot (dir, nickname, version,production) values ('/reg/neh/home5/jrb/caqtdm_jrbfork/caQtDM_Viewer', 'hometest','noversion',0)
+
+insert into FileRoot (dir, nickname, version) values ('/reg/g/pcds/package/epics/3.14-dev/screens/edm/xcs/current', 'xcs','3.14-dev')
+insert into FileRoot (dir, nickname, version) values ('/reg/g/pcds/package/epics/3.14-dev/screens/edm/common/current', 'common','3.14-dev')
--- writedb.py
+++ writedb.py
@@ -3,7 +3,8 @@
 '''
 from __future__ import print_function
 from sqlalchemy import engine, create_engine
-
+from os import path
+from time import mktime,localtime
 
 def _getdburl(file):
     kwds = {}
@@ -23,7 +24,8 @@ class HandleMeasurement(object):
         # for connect_file use .ssh/stats_alchemy.txt
         self.meas_id=id
         self.meas_name=nickname
-        self.root_dir=None
+        self.rootdir=None
+        self.rootdir_id=None
 
         # get db connection
         db_url = _getdburl(connect_file)
@@ -31,41 +33,81 @@ class HandleMeasurement(object):
 
         if id != 0:
             # Check it's there; fetch nickname
-            q = 'select FR.nickname as nick,FR.dir from Measurement M join FileRoot FR on M.fileRootId=FR.id where M.id=' + str(id)
+            q = 'select FR.nickname as nick,FR.dir,FR.id as frid from Measurement M join FileRoot FR on M.fileRootId=FR.id where M.id=' + str(id)
             r = self.engine.execute(q)
             row = r.fetchone()
             if row == None:
                 raise Exception("No such id in Measurement table")
             self.nickname = row['nick']
-            self.root_dir = row['dir']
+            self.rootdir = row['dir']
+            self.rootdir_id = row['frid']
             print('Found nickname ' + self.nickname)
         else:
             if nickname is not None:
                 # Check if it's there; fetch id
-                q = 'select M.id as mid,FR.dir from Measurement M join FileRoot FR on M.fileRootId=FR.id where nickname="' + nickname + '"'
+                q = 'select M.id as mid,FR.dir,FR.id as frid from Measurement M join FileRoot FR on M.fileRootId=FR.id where nickname="' + nickname + '"'
                 r = self.engine.execute(q)
                 row = r.fetchone()
                 if row == None:
                     raise Exception("No row with nickname " + nickname)
 
                 self.meas_id = row['mid']
-                self.root_dir = row['dir']
+                self.rootdir = row['dir']
+                self.rootdir_id = row['frid']
                 print('Found id ' + str(self.meas_id))
                 
                 row = r.fetchone()
                 if row != None:
                     raise Exception("No unique row with nickname " + nickname)
 
-        print("Root dir is: ",self.root_dir)
+        #print("Root dir is: ",self.root_dir)
 
-    def scanFiles(self):
-      '''
-      Look for edl files associated with our measurement context. 
-      Make entries in File and MappingFileMeas tables
-      '''
-      from .getStats import get_stats
+    def scan_files(self):
+        '''
+        Look for edl files associated with our measurement context. 
+        Make entries in File and MappingFileMeas tables
+        '''
+        from getStats import get_stats
+
+        file_entries = get_stats('*.edl', self.rootdir, fmt='%n?%Y?%X?%x')
+        print("#Entries found: ",len(file_entries))
+        if len(file_entries) > 0:
+            print("First entry: ",file_entries[0][0])
+            print("Last modified: ", file_entries[0][1])
+            print("Last accessed (count): ", file_entries[0][2])
+            print("Last accessed (ascii ts): ", file_entries[0][3])
+        # For each file make an entry in File and one in MappingFileMeas
+
+        connect = self.engine.connect()
+        with connect.begin() as trans:
+
+            first = True
+            for e in file_entries:
+                basename = path.basename(e[0])
+                unusedSec = mktime(localtime()) - int(e[2])
+                qf = "insert into File (fileRootId, relPath,basename,modifyTimeU,lastAccessU,unused) values ('" + str(self.rootdir_id) + "','" + e[0] + "','" + basename + "','"
+                qf += str(e[1]) + "','" + str(e[2]) + "','" + str(unusedSec) + "')"
+                if first:
+                    print("Insert into File is: ")
+                    print(qf)
+
+                r = connect.execute(qf)
+
+                last_insert = connect.execute('select last_insert_id()')
+                last_id = last_insert.first()[0]
+                qm = "insert into MappingFileMeas (fileId,measurementId) values ('"
+                qm += str(last_id) + "','" + str(self.meas_id) + "')"
+                if first:
+                    print("Insert into MappingFileMeas is: ")
+                    print(qm)
+                    first = False
+                connect.execute(qm)
+                #if not first: break
+        connect.close()
       
 if __name__ == '__main__':
     manager = HandleMeasurement('/reg/neh/home5/jrb/.ssh/stats_alchemy.txt', 
-                                nickname='mec')
-    print("Instantiated")
+                                nickname='xpp')
+    #print("Instantiated")
+    manager.scan_files()
+
